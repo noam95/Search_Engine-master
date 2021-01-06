b7303a0ba16d22a9272eeb84eb4bbba0c5ce5f28
Aviran <avirangi@post.bgu.ac.il>
2021-01-06 19:32:00 +0200

    improve word2vec ranker

--- parser_module_adv.py
+++ parser_module_adv.py
@@ -116,7 +116,7 @@ class Parse_ADV(Parse):
 
     def numbers_to_int(self,num):
 
-        numbers = ['one','two','three','for','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen', \
+        numbers = ['zero','one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen', \
              'seventeen','eighteen','nineteen','twenteen']
         if num in numbers:
             num = numbers.index(num)
